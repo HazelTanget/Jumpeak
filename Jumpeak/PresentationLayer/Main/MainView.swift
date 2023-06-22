5371f0cb415d82f32270af44bad94ac0af9809a5
Bolshachkov Denis <bolshachkov@bifit.com>
2023-06-22 22:43:34 +0300

    Finish main view ui

--- Jumpeak/PresentationLayer/Main/MainView.swift
+++ Jumpeak/PresentationLayer/Main/MainView.swift
@@ -10,6 +10,7 @@ import CardStack
 
 struct MainView: View {
     @State private var selection = 0
+    @State private var shouldShowProffessions = false
     
     var vacancy: Vacancy?
     @State private var cards = [Vacancy(id: UUID(), title: "sdfsda", description: "sfdfds", cardImage: Image("bg"), logo: Image("bg"), skills: [HardSkill(name: "Пить пиво")], salary: 75000)]
@@ -40,7 +41,59 @@ struct MainView: View {
     }
     
     var main: some View {
-        VStack {
+        VStack (spacing: 0) {
+            HStack {
+                VStack {
+                    Menu {
+                        Button {
+                            // do something
+                        } label: {
+                            Text("Linear")
+                        }
+                        Button {
+                            // do something
+                        } label: {
+                            Text("Radial")
+                        }
+                    } label: {
+                        HStack {
+                            Text("Разработчик базы данных")
+                                .lFont()
+                                .foregroundColor(Asset.Colors.mainFontColor.swiftUIColor)
+
+                            Spacer()
+
+                            Image(systemName: "chevron.down")
+                                .foregroundColor(Asset.Colors.mainFontColor.swiftUIColor)
+
+                        }
+                        .padding(.vertical, 12)
+                        .padding(.horizontal, 16)
+                        .background(Asset.Colors.inputColor.swiftUIColor)
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                        .padding(.leading, 16)
+                        .padding(.trailing, 8)
+
+                    }
+                }
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "bell")
+                        .foregroundColor(Asset.Colors.mainFontColor.swiftUIColor)
+                }
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(Asset.Colors.inputColor.swiftUIColor)
+                .clipShape(Circle())
+                .padding(.trailing, 16)
+
+            }
+            .padding(.top, 0.3)
+            
+            Spacer()
+
             CardStack(
                 direction: LeftRight.direction, // See below for directions
                 data: cards,
@@ -55,6 +108,73 @@ struct MainView: View {
                     
                 }
             )
+            .padding(.top, 16)
+            
+            HStack (spacing: 8) {
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "xmark")
+                            .background(Asset.Colors.background.swiftUIColor)
+                            .foregroundColor(Asset.Colors.errorColor.swiftUIColor)
+                            .clipShape(Circle())
+                        
+                        Text("Скипнуть")
+                            .mFont()
+                            .foregroundColor(Asset.Colors.mainFontColor.swiftUIColor)
+                    }
+                }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 16)
+                .background(Asset.Colors.inputColor.swiftUIColor)
+                .clipShape(RoundedRectangle(cornerRadius: 24))
+                
+//                Button {
+//
+//                } label: {
+//                    HStack {
+//                        Image(systemName: "info.circle")
+//                            .fontWeight(.medium)
+//                            .foregroundColor(Asset.Colors.mainFontColor.swiftUIColor)
+//                            .background(Asset.Colors.background.swiftUIColor)
+//
+//                            .clipShape(Circle())
+//                    }
+//                }
+//                .padding(.horizontal, 16)
+//                .padding(.vertical, 16)
+//                .background(Asset.Colors.inputColor.swiftUIColor)
+//                .clipShape(RoundedRectangle(cornerRadius: 24))
+                
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Image(systemName: "hand.thumbsup")
+                            .background(Asset.Colors.background.swiftUIColor)
+                            .foregroundColor(Asset.Colors.successColor.swiftUIColor)
+                            .clipShape(Circle())
+                        
+                        Text("Нравится")
+                            .mFont()
+                            .foregroundColor(Asset.Colors.mainFontColor.swiftUIColor)
+                    }
+                }
+                .padding(.horizontal, 16)
+                .padding(.vertical, 16)
+                .background(Asset.Colors.inputColor.swiftUIColor)
+                .clipShape(RoundedRectangle(cornerRadius: 24))
+            }
+            .background(Asset.Colors.background.swiftUIColor)
+            .padding(.bottom, 16)
+            
+            
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Asset.Colors.inputColor.swiftUIColor)
+                .frame(height: 1)
+                .padding(.bottom, 8)
+            
         }
         .background(Asset.Colors.background.swiftUIColor)
     }
